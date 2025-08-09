43c1ce89d7f2e58abc79285201cb6a1bc8958f25
thrinath862 <thrinathreddyallu@gmail.com>
2025-08-09 10:43:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
